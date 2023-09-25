4ea363a9aee9c42bcb20b2418d452f3e89c99fa3
mranderson <leon.avshalom@gmail.com>
2023-09-26 01:30:24 +0300

    Working DB initial insertion

--- services/web/entrypoint.sh
+++ services/web/entrypoint.sh
@@ -4,13 +4,11 @@ if [ "$DATABASE" = "postgres" ]
 then
     echo "Waiting for postgres..."
 
-    while ! nc -z $SQL_HOST $SQL_PORT; do
+    while ! nc -z $DB_HOST $DB_PORT; do
       sleep 0.1
     done
 
     echo "PostgreSQL started"
 fi
-
-python manage.py create_db
 
 exec "$@"
