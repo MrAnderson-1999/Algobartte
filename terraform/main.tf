7612ce13a1a413390f1f173a925e8b0a3fc5e7a0
mranderson <leon.avshalom@gmail.com>
2023-09-26 05:11:46 +0300

    Merged with backend, working prod/dev with db

--- terraform/main.tf
+++ terraform/main.tf
@@ -14,15 +14,6 @@ provider "aws" {
 }
 
 
-provider "google" {
-credentials =  file("/home/leon/sonorous-summer-376712-419c4adc4106.json")
-
-  project = var.gcp_account_id
-  region  = "us-central1"
-  zone    = "us-central1-c"
-}
-
-
 resource "aws_instance" "app_server" {
   ami           = "ami-0aa7d40eeae50c9a9"
   instance_type = "t2.micro"
@@ -31,6 +22,3 @@ resource "aws_instance" "app_server" {
     Name = var.aws_instance_name
   }
 }
-
-resource "google_compute_network" "vpn_network" {
-  name = "terraform-network"
